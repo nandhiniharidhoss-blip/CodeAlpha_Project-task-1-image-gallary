4197af83a4c282a1824e377913096c8c094448cd
lamiyalakxxz <nandhiniharidhoss@gmail.com>
2026-07-22 23:02:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -51,6 +51,43 @@ else{
 card.style.display="none";
 
 }
+  const images = document.querySelectorAll(".gallery .card img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+let currentIndex = 0;
+
+// Open image
+images.forEach((img, index) => {
+    img.addEventListener("click", () => {
+        currentIndex = index;
+        lightbox.style.display = "flex";
+        lightboxImg.src = images[currentIndex].src;
+    });
+});
+
+// Previous
+document.querySelector(".prev").addEventListener("click", () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    }
+    lightboxImg.src = images[currentIndex].src;
+});
+
+// Next
+document.querySelector(".next").addEventListener("click", () => {
+    currentIndex++;
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+    lightboxImg.src = images[currentIndex].src;
+});
+
+// Cancel / Close
+document.querySelector(".close").addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
 
 });
 
